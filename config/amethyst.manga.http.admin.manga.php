9f2e3758326f3ba7abbc5db5820b439a374f9774
railken <railken@protonmail.ch>
2019-01-21 19:57:34 +0100

    fix style

--- config/amethyst.manga.http.admin.manga.php
+++ config/amethyst.manga.http.admin.manga.php
@@ -1,10 +1,10 @@
 <?php
 
 return [
-    'enabled'     => true,
-    'controller'  => Railken\Amethyst\Http\Controllers\Admin\MangaController::class,
-    'router'      => [
-        'as'        => 'manga.',
-        'prefix'    => '/manga',
+    'enabled'    => true,
+    'controller' => Railken\Amethyst\Http\Controllers\Admin\MangaController::class,
+    'router'     => [
+        'as'     => 'manga.',
+        'prefix' => '/manga',
     ],
 ];
