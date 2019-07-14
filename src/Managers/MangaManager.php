471e3e234025561c733497b28135fb9cd3b12f11
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/MangaManager.php
+++ src/Managers/MangaManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Manga newEntity()
+ * @method \Amethyst\Schemas\MangaSchema getSchema()
+ * @method \Amethyst\Repositories\MangaRepository getRepository()
+ * @method \Amethyst\Serializers\MangaSerializer getSerializer()
+ * @method \Amethyst\Validators\MangaValidator getValidator()
+ * @method \Amethyst\Authorizers\MangaAuthorizer getAuthorizer()
+ */
 class MangaManager extends Manager
 {
     use ConfigurableManager;
