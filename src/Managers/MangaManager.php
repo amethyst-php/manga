e5a5bf887fb512dd60b9ea88edded4ea873fc097
railken <railken@protonmail.ch>
2019-07-14 13:09:57 +0200

    fix style

--- src/Managers/MangaManager.php
+++ src/Managers/MangaManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\Manga newEntity()
- * @method \Amethyst\Schemas\MangaSchema getSchema()
+ * @method \Amethyst\Models\Manga                 newEntity()
+ * @method \Amethyst\Schemas\MangaSchema          getSchema()
  * @method \Amethyst\Repositories\MangaRepository getRepository()
- * @method \Amethyst\Serializers\MangaSerializer getSerializer()
- * @method \Amethyst\Validators\MangaValidator getValidator()
- * @method \Amethyst\Authorizers\MangaAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\MangaSerializer  getSerializer()
+ * @method \Amethyst\Validators\MangaValidator    getValidator()
+ * @method \Amethyst\Authorizers\MangaAuthorizer  getAuthorizer()
  */
 class MangaManager extends Manager
 {
